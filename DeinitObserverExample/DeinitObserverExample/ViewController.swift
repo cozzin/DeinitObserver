115d9197767a69b9c1dec84db7f9867386bfd453
seongho.hong <hsh3592@gmail.com>
2021-09-21 21:40:02 +0900

    Add Example

---
+++ DeinitObserverExample/DeinitObserverExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeinitObserverExample
+//
+//  Created by seongho.hong on 2021/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Sources/DeinitObserver/DeinitObserver.swift
+++ Sources/DeinitObserver/DeinitObserver.swift
@@ -1,3 +1,16 @@
-struct DeinitObserver {
-    var text = "Hello, World!"
+import Foundation
+
+final class DeinitObserver {
+    
+    typealias DidDeinit = () -> Void
+    
+    private let didDeinit: DidDeinit
+    
+    init(didDeinit: @escaping DidDeinit) {
+        self.didDeinit = didDeinit
+    }
+    
+    deinit {
+        didDeinit()
+    }
 }
